1e21fb10e20460baafc7876ca02098011f60a1ae
codewithshiga <your@email.com>
2025-06-27 00:05:59 +0530

    Updated all files with new changes

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Welcome to DevNest Eco App!");
+git add .
+git commit -m "Restore index.html for GitHub Pages"
+git push origin main
+
+git add .
+git commit -m "Updated homepage content"    # or any clear message
+git push origin main
+
